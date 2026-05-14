d84a285d6b5aac8def761de3662b08a8c156c1d3
Антон Федосеев <rain.playyt@yandex.ru>
2026-05-14 17:13:45 +0300

    Добавлено задание 2 и 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,146 @@
 ﻿using System.Linq.Expressions;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-NewMethod();
+//Task1();
+//Task2();
+Task3();
 
-static void NewMethod()
+
+
+
+static void Task3()
+{
+
+    try
+    {
+
+        bool exit = true;
+        while (exit)
+        {
+            decimal kpiBonus = 0;
+
+            Console.WriteLine("\nВведите Зароботную плату");
+            string summaryString = Console.ReadLine();
+            if (decimal.TryParse(summaryString, out decimal summaryDecimal)) { }
+            else
+            {
+                if (summaryString.Trim().ToLower() == "exit") { exit = false; break; }
+                else
+                {
+                    Console.WriteLine("Введена не цифра, для корректной работы использйте цифры \n");
+                    continue;
+                }
+
+            }
+
+            Console.WriteLine("\nВведите KPI в процентах");
+            string kpiString = Console.ReadLine();
+            if (int.TryParse(kpiString, out int kpiInt))
+            {
+                if (kpiInt >= 90) { kpiBonus = summaryDecimal * 0.2m; }
+                else if (kpiInt >= 75 && kpiInt <= 89) { kpiBonus = summaryDecimal * 0.1m; }
+                else { kpiBonus = 0; }
+
+            }
+            else
+            {
+                if (kpiString.Trim().ToLower() == "exit") { exit = false; break; }
+                else
+                {
+                    Console.WriteLine("Введена не цифра, для корректной работы использйте цифры \n");
+                    continue;
+                }
+
+            }
+
+            Console.WriteLine($"Ваша зп равна {summaryDecimal + kpiBonus}");
+
+
+        }
+
+
+        
+
+
+
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Возникла ошибка {ex.Message}");
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static void Task2()
+{
+    try
+    {
+
+        bool exit = true;
+        while (exit)
+        {
+            Console.WriteLine("\nВведите возраст покупателя");
+            string ageString = Console.ReadLine();
+            if (int.TryParse(ageString, out int ageInt))
+            {
+                if (ageInt >= 18) { Console.WriteLine($"Покупатель совершеннолетний"); } else { Console.WriteLine("Покупатель несовершеннолетний"); }
+
+
+            }
+            else
+            {
+                if (ageString.Trim().ToLower() == "exit") { exit = false; break; }
+                else
+                {
+                    Console.WriteLine("Введена не цифра, для корректной работы использйте цифры \n");
+                    continue;
+                }
+
+            }
+
+
+        }
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Возникла ошибка {ex.Message}");
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+static void Task1()
 {
     string resultMessage = "Ваш ответ равен ";
     try
