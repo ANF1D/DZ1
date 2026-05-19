3ec7abf82c3442756c572dbe0775900e20d3d744
Антон Федосеев <rain.playyt@yandex.ru>
2026-05-19 16:32:22 +0300

    задание 2

--- Dz2/Program.cs
+++ Dz2/Program.cs
@@ -1,36 +1,66 @@
 ﻿using System.Diagnostics;
 using System.Linq.Expressions;
 
-decimal discount = default, finalAmount = default;
-bool isVip = true, isFirstOrder = true, hasPromo = true, isFreeDelivery = default;
+//decimal discount = default, finalAmount = default;
+//bool isVip = true, isFirstOrder = true, hasPromo = true, isFreeDelivery = default;
 
-Console.WriteLine("Введите сумму заказа");
-if (!decimal.TryParse(Console.ReadLine(), out decimal amount))
+//Console.WriteLine("Введите сумму заказа");
+//if (!decimal.TryParse(Console.ReadLine(), out decimal amount))
+//{
+//    Console.WriteLine("сумма введене не корректно");     
+//}
+
+//finalAmount += amount;
+
+//Console.WriteLine("Введите ");
+
+//if (isFirstOrder)       discount += 0.05m; 
+//if (hasPromo)           discount += 0.1m; 
+//if (isVip)              discount += 0.07m; 
+//if (discount >= 0.2m)   discount = 0.2m;
+//if (amount >= 15000m)
+//{
+//    isFreeDelivery = true;
+//}
+//else
+//{
+//    isFreeDelivery = false;
+//    finalAmount += 1200m;
+//}
+
+//string deliveryPrice = isFreeDelivery ? "Бесплатно" : "1200";
+//finalAmount -= amount * discount;
+
+//Console.WriteLine($"Итоговая скидка: {discount * 100}%");
+//Console.WriteLine($"Стоимость доставки: {deliveryPrice}");
+//Console.WriteLine($"Финальная сумма к оплате: {finalAmount}");
+
+decimal summary = default, net = default, tax = 0.1m;
+double dayRate = 5, nightRate = 10, overtime;
+
+Console.WriteLine("Введите количество часов в дневной смене");
+if (!double.TryParse(Console.ReadLine(), out double dayHours))
 {
-    Console.WriteLine("сумма введене не корректно");     
+    Console.WriteLine("Значение введено не корректно");
+}
+Console.WriteLine("Введите количество часов в ночной смене");
+if (!double.TryParse(Console.ReadLine(), out double nightHours))
+{
+    Console.WriteLine("Значение введено не корректно");
 }
 
-finalAmount += amount;
-
-Console.WriteLine("Введите ");
-
-if (isFirstOrder)       discount += 0.05m; 
-if (hasPromo)           discount += 0.1m; 
-if (isVip)              discount += 0.07m; 
-if (discount >= 0.2m)   discount = 0.2m;
-if (amount >= 15000m)
+Console.WriteLine("Введите weekendShift");
+if (!bool.TryParse(Console.ReadLine(), out bool weekendShift))
 {
-    isFreeDelivery = true;
+    Console.WriteLine("Значение введено не корректно, надо true или false");
 }
-else
-{
-    isFreeDelivery = false;
-    finalAmount += 1200m;
-}
+double allHours = dayHours + nightHours;
+summary = (decimal)(dayHours * dayRate + nightHours * nightRate);
 
-string deliveryPrice = isFreeDelivery ? "Бесплатно" : "1200";
-finalAmount -= amount * discount;
+if (allHours > 160) overtime = allHours - 160 * dayRate * 1.5;
+if (weekendShift) summary += summary * 0.5m;
 
-Console.WriteLine($"Итоговая скидка: {discount * 100}%");
-Console.WriteLine($"Стоимость доставки: {deliveryPrice}");
-Console.WriteLine($"Финальная сумма к оплате: {finalAmount}");
+net = summary - (summary * tax);
+
+Console.WriteLine($"Gross = {summary}");
+Console.WriteLine($"Net = {net}");
