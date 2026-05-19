d77edd6d3008202f61f202b8412f79e73fd2b778
Антон Федосеев <rain.playyt@yandex.ru>
2026-05-19 15:40:56 +0300

    1 задание

--- Dz2/Program.cs
+++ Dz2/Program.cs
@@ -1,16 +1,24 @@
-﻿decimal discount = default, finalAmount = default;
-bool isVip = default, isFirstOrder = default, hasPromo = default, isFreeDelivery = default;
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
 
+decimal discount = default, finalAmount = default;
+bool isVip = true, isFirstOrder = true, hasPromo = true, isFreeDelivery = default;
+
+Console.WriteLine("Введите сумму заказа");
 if (!decimal.TryParse(Console.ReadLine(), out decimal amount))
 {
-    Console.WriteLine("сумма введене не корректно");
+    Console.WriteLine("сумма введене не корректно");     
 }
 
-if (isVip && discount <= 0.2m) { discount += 0.07m; }
-if (isFirstOrder && discount <= 0.2m) { discount += 0.05m; }
-if (hasPromo && discount <= 0.2m) { discount += 0.1m; }
+finalAmount += amount;
 
-if (amount >= 15000)
+Console.WriteLine("Введите ");
+
+if (isFirstOrder)       discount += 0.05m; 
+if (hasPromo)           discount += 0.1m; 
+if (isVip)              discount += 0.07m; 
+if (discount >= 0.2m)   discount = 0.2m;
+if (amount >= 15000m)
 {
     isFreeDelivery = true;
 }
@@ -21,9 +29,8 @@ else
 }
 
 string deliveryPrice = isFreeDelivery ? "Бесплатно" : "1200";
+finalAmount -= amount * discount;
 
-finalAmount += amount * discount;
-
-Console.WriteLine($"Итоговая скидка: {discount * 100}");
+Console.WriteLine($"Итоговая скидка: {discount * 100}%");
 Console.WriteLine($"Стоимость доставки: {deliveryPrice}");
-Console.WriteLine($"Финальная сумма к оплате{finalAmount}");
+Console.WriteLine($"Финальная сумма к оплате: {finalAmount}");
